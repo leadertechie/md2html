feecc69df5c9c62a941da679639ea7ac0310a509
leadertechie <leadertechie@gmail.com>
2026-03-29 19:22:04 +0800

    fix: use vite for bundling to fix ESM import issues (#6)
    Co-authored-by: Milu <milu@techieleader.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 import { resolve } from 'path';
 
 export default defineConfig({
+  plugins: [
+    dts({
+      rollupTypes: true,
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
