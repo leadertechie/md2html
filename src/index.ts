bd76ab34830d3af478c1bb402f9f7fca8ecdbc69
leadertechie <leadertechie@gmail.com>
2026-03-29 21:25:10 +0800

    Docs/add peerdep note (#8)
    * docs: add peerDependency note to installation
    
    * Separate lit-renderer to avoid bundling lit in worker
    
    ---------
    
    Co-authored-by: Milu <milu@techieleader.com>

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 export * from './types.js';
 export * from './parser.js';
 export * from './renderer.js';
-export * from './lit-renderer.js';
 export * from './pipeline.js';
-
-export { LitRenderer as HTMLRenderer } from './lit-renderer.js';
